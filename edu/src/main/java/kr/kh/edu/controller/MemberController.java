2f4f01f13e8bc4b69464b3f19a7e3ac338af9213
오제 <heyoje325@gmail.com>
2023-09-24 15:47:01 +0900

    edu : DB연동 및 패키지 세팅

---
+++ edu/src/main/java/kr/kh/edu/controller/MemberController.java
@@ -0,0 +1,8 @@
+package kr.kh.edu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+}
---
+++ edu/src/main/java/kr/kh/edu/vo/MemberVO.java
@@ -0,0 +1,5 @@
+package kr.kh.edu.vo;
+
+public class MemberVO {
+
+}

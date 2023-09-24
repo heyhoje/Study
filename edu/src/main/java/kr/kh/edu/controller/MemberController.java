5b9e421e0e6c1c2e3209843ef7bc467495d330bf
오제 <heyoje325@gmail.com>
2023-09-24 16:22:24 +0900

    edu : * 놓치고 있던 부분 * 추가
    - pom.xml : 버전업, @Data - 룸북 의존성 추가
    - mapper / root-context.xml 베이스패키지 경로 수정
           kr.kh.edu.dao
    - servlet-context.xml : 베이스패키지 뒤에 .* 붙여야함

--- edu/src/main/java/kr/kh/edu/controller/MemberController.java
+++ edu/src/main/java/kr/kh/edu/controller/MemberController.java
@@ -1,8 +1,13 @@
 package kr.kh.edu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import kr.kh.edu.service.MemberService;
 
 @Controller
 public class MemberController {
 
+	@Autowired
+	MemberService memberService;
 }
--- edu/src/main/java/kr/kh/edu/service/MemberServiceimp.java
+++ edu/src/main/java/kr/kh/edu/service/MemberServiceimp.java
@@ -1,8 +1,13 @@
 package kr.kh.edu.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.kh.edu.dao.MemberDAO;
 
 @Service
 public class MemberServiceimp implements MemberService{
 
+	@Autowired
+	MemberDAO memberDao;
 }

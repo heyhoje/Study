338c73110cc1582c2cbc852dfbdedcdf04308f40
오제 <heyoje325@gmail.com>
2023-12-25 02:37:12 +0900

    nadocoing : 쓰레드 Quiz

---
+++ JavaWorkspace/src/chap_12/_Quiz_12.java
@@ -0,0 +1,20 @@
+package chap_12;
+
+// A 상품 준비 1/5
+// B 상품 준비 1/5
+//    ...
+// -- A 상품 준비 완료 --
+// -- B 상품 준비 완료 --
+// == 세트 상품 포장 시작 ==
+// 세트 상품 포장 1/5
+//    ...
+// == 세트 상품 포장 완료 ==
+
+public class _Quiz_12 {
+    public static void main(String[] args) {
+        // 상품 A를 준비하는 Runnable 클래스
+        // Runnable runnableA = new Runnable(){
+
+        // };
+    }
+}
